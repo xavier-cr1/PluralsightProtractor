827acd1f949562cfa51d9a4e9bc091d7f8baca5c
Xavier Casafont <x.casafont@safened.com>
2019-10-08 17:29:35 +0200

    testing

--- protractor/ratings/event.list.item.spec.js
+++ protractor/ratings/event.list.item.spec.js
@@ -2,6 +2,14 @@ describe('Event List Details: ', function(){
     describe('When clicking on an event', function(){
         it('Should navigate to the details page', function(){
             browser.get('http://localhost:3000/#!/EventRatings');
+
+            var firstElement = element.all(by.binding('name')).first();
+
+            var name;
+            firstElement.getText().then(function(text){
+                name = text;
+            });
+
         })
     })
 });
