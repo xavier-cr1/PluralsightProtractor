5500bfc10b226158e6550b533f07ed2e3ce8da06
Xavier Casafont <x.casafont@safened.com>
2019-10-08 11:39:14 +0200

    adding new protractor test project

---
+++ protractor/ratings/event.list.item.spec.js
@@ -0,0 +1,7 @@
+describe('Event List Details: ', function(){
+    describe('When clicking on an event', function(){
+        it('Should navigate to the details page', function(){
+            browser.get('http://localhost:3000/#!/EventRatings');
+        })
+    })
+});
